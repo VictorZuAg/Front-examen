6f64a23fdea6646265a36a7d069c79bf754ca024
Victor Manuel Zuñiga Aguilar <vick18v@gmail.com>
2018-05-23 15:00:24 -0500

    Union de Back con Front

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
         if(this.state.usuarios.length > 0){
           return(
             <div className="App">
-            <input type="text" placeholder="Search" value={this.state.searchString} onChange={this.handleChange}/>
+            {/* <input type="text" placeholder="Search" value={this.state.searchString} onChange={this.handleChange}/> */}
               {/* {this.state.usuarios.map(usuario => <User id={usuario.id} name={usuario.nombre} email={usuario.correo}/>)} */}
               {this.state.usuarios.map(usuario => <li><Link to={{pathname: usuario.id, state:{datos:usuario}}} >{usuario.nombre}</Link></li>)}
               <Content body={children}/>
